2d170c8c9a9ba593c078faacdecd247bc3063c91
AnasKhan1506 <akanaskhan1506@gmail.com>
2023-11-23 19:05:39 -0800

    auth connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
